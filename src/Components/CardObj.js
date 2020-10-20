f2d1286b603fe17112c9a4940276648639da0179
aghyad.jabali <aghyadjabali2gmail.com>
2020-10-20 16:49:19 +0200

    Match update

--- src/Components/CardObj.js
+++ src/Components/CardObj.js
@@ -4,12 +4,10 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Block, CenterFocusStrong, FullscreenExit } from '@material-ui/icons';
-import { blue, red } from '@material-ui/core/colors';
-import ListItem from '@material-ui/core/ListItem';
+
 
 const useStyles = makeStyles({
   root: {
@@ -44,27 +42,29 @@ const useStyles = makeStyles({
 
 
 
-export default function CardObj({d,getMatchTwo}) {
+export default function CardObj({ d, getMatchTwo }) {
   const classes = useStyles();
 
   return (
 
     <Card className={classes.root}>
-      <CardActionArea>
+    
 
         <CardContent>
           <div>
-            <Typography variant="h6" className={classes.team} component="p" noWrap>
+         
+            <a href={"/team/" + d.homeTeam.id} variant="h6" className={classes.team} component="p" noWrap>
               {d.homeTeam.name}
-            </Typography>
+            </a>
+           
             <Typography variant="h6" className={classes.score} component="p" noWrap>
               {d.result.homeScore}
             </Typography>
           </div>
           <div>
-            <Typography variant="h6" className={classes.team} component="p" noWrap>
+            <a href={"/team/" + d.homeTeam.id} variant="h6" className={classes.team} component="p" noWrap>
               {d.awayTeam.name}
-            </Typography>
+            </a>
             <Typography variant="h6" className={classes.score} component="p" noWrap>
               {d.result.awayScore}
             </Typography>
@@ -77,10 +77,10 @@ export default function CardObj({d,getMatchTwo}) {
           </Typography>
 
         </CardContent>
-      </CardActionArea>
+
       <CardActions>
-        <Button  onClick = {()=>getMatchTwo(d.awayTeam.id,d.homeTeam.id)} size="small">
- 
+        <Button onClick={() => getMatchTwo(d.awayTeam.id, d.homeTeam.id)} size="small">
+
           More Matches
         </Button>
       </CardActions>
