3f66912522276e5101f1128a3b1895826ff79e2c
James Morgan <james.d.morgan9830@gmail.com>
2020-10-20 16:43:25 +0200

    added teams matches to team

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import api from './api'
+import CardObj from './CardObj';
 import StandingTable from './StandingTable';
 
 function Team({ match: { params: { id } } }) {
@@ -12,56 +13,55 @@ function Team({ match: { params: { id } } }) {
     standing: []
   });
 
-
   useEffect(() => {
+
     api.getGamesByTeamId(id).then(res => {
       let teamName
+
       if (res.data.length > 0) {
-        if (res.data[0].homeTeam.id == state.id) {
-          teamName = res.data[0].homeTeam.name 
+        if (res.data[0].homeTeam.id == id) {
+          teamName = res.data[0].homeTeam.name
         } else {
-          teamName = res.data[0].awayTeam.name 
+          teamName = res.data[0].awayTeam.name
         }
         api.getTableBySeasonId(res.data[0].round.season.id).then(tableRes => {
-          tableRes.data.every(element => {
-            if (element.teamId == state.id) {
-              setState({ 
-                isLoaded: true,
-                teamName: teamName,
-                matches: res.data,
-                standing: element 
-              })
-              return false
-            }
-          });
-        }, error => {
+
           setState({
             isLoaded: true,
+            teamName: teamName,
+            matches: res.data,
+            standing: tableRes.data
+          })
+
+        }, error => {
+          setState({
+            isLoaded: false,
             error: error,
           })
         })
       }
     }, error => {
       setState({
-        isLoaded: true,
+        isLoaded: false,
         error: error,
       })
     })
-
-
-
     // eslint-disable-next-line
   }, [])
-  
-  if (state.standing != null && state.teamName != null) {
+
+  if (state.isLoaded) {
+    console.log(state)
     return (
       <div>
-        {console.log(state.teamName)}
         <h1>{state.teamName}</h1>
-        <StandingTable items={[state.standing]} />
+        <StandingTable items={state.standing} />
+        {state.matches.map((match, i) => (
+          <CardObj key={i} d={match} getMatchTwo={(x,y)=>{return null}} />
+        ))}
       </div>
     )
   } else {
+    console.log(state)
     return null;
   }
 }
