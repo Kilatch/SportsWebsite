ddd47804ce91bafc25eb197baf04f5c9fca38f87
James Morgan <james.d.morgan9830@gmail.com>
2020-10-22 15:23:34 +0200

    table desigen

--- src/Components/StandingTable.js
+++ src/Components/StandingTable.js
@@ -7,10 +7,44 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import styles from './styles/Table.css'
 
 export default function StandingTable(props) {
   if (props.items != null && props.items.length > 0) {
     return (
+      <table>
+        <thead>
+          <tr>
+            <th>Team</th>
+            <th>MP</th>
+            <th>W</th>
+            <th>D</th>
+            <th>L</th>
+            <th className='goals'>G</th>
+            <th className='goals'>GC</th>
+            <th className='goals'>GD</th>
+            <th>P</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.items.map(row => (
+            <tr key={row.teamId}>
+              <td>{row.teamName}</td>
+              <td>{row.gamesPlayed}</td>
+              <td>{row.wins}</td>
+              <td>{row.draws}</td>
+              <td>{row.loss}</td>
+              <td className='goals'>{row.goals}</td>
+              <td className='goals'>{row.goalsConseded}</td>
+              <td className='goals'>{row.goalDifference}</td>
+              <td>{row.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+    /*
+    (
       <TableContainer component={Paper}>
         <MatTable aria-label="simple table">
           <TableHead>
@@ -43,7 +77,7 @@ export default function StandingTable(props) {
           </TableBody>
         </MatTable>
       </TableContainer>
-    )
+    )*/
   } else {
     return null;
   }
