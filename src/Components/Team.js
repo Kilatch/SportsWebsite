da92ccc7dff550592603b2b33b4dbd81e22c24b2
James Morgan <james.d.morgan9830@gmail.com>
2020-10-28 10:43:04 +0100

    team caching

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -2,76 +2,74 @@ import React, { useState, useEffect } from 'react'
 import api from './api'
 import CardObj from './CardObj';
 import { Grid } from '@material-ui/core';
-import StandingTable from './StandingTable';
+import LoadTable from './LoadTable';
 import Button from '@material-ui/core/Button';
 
 function Team({ match: { params: { id } } }) {
-  const [state, setState] = useState({
-    error: null,
-    isLoaded: false,
-    id: id,
-    teamName: '',
-    matches: [],
-    homeMatches: [],
-    awayMatches: [],
-    show: "all",
-    standing: []
-  });
+  let cachKey = "teamDetail:" + id
+  const [state, setState] = useState(
+    JSON.parse(sessionStorage.getItem(cachKey)) ||
+    {
+      error: null,
+      isLoaded: false,
+      id: id,
+      teamName: '',
+      matches: [],
+      seasonId: '',
+      show: "all"
+    });
 
   useEffect(() => {
+    if (!state.isLoaded) {
+      api.getGamesByTeamId(id).then(res => {
+        let teamName
 
-    api.getGamesByTeamId(id).then(res => {
-      let teamName
-
-      if (res.data.length > 0) {
-        if (res.data[0].homeTeam.id == id) {
-          teamName = res.data[0].homeTeam.name
-        } else {
-          teamName = res.data[0].awayTeam.name
+        if (res.data.length > 0) {
+          if (res.data[0].homeTeam.id == id) {
+            teamName = res.data[0].homeTeam.name
+          } else {
+            teamName = res.data[0].awayTeam.name
+          }
+          let tmp = { ...state }
+          tmp.isLoaded = true
+          tmp.teamName = teamName
+          tmp.matches = res.data
+          tmp.show = "all"
+          tmp.seasonId = res.data[0].round.season.id
+          setState(tmp)
+          sessionStorage.setItem(cachKey, JSON.stringify(tmp))
         }
-        api.getTableBySeasonId(res.data[0].round.season.id).then(tableRes => {
-          setState({
-            isLoaded: true,
-            teamName: teamName,
-            matches: res.data,
-            standing: tableRes.data,
-            show: "all"
-          })
-        }, error => {
-          setState({
-            isLoaded: false,
-            error: error,
-          })
+      }, error => {
+        setState({
+          isLoaded: false,
+          error: error,
         })
-      }
-    }, error => {
-      setState({
-        isLoaded: false,
-        error: error,
       })
-    })
+    }
     // eslint-disable-next-line
   }, [])
 
   let matches = "all";
 
-  let MatchGrid 
+  let MatchGrid
 
   let getAwayMatches = () => {
     api.getAwayGamesByTeamId(id).then(res => {
-      let tmp = {...state}
+      let tmp = { ...state }
       tmp.awayMatches = res.data
       tmp.show = "away"
       setState(tmp)
+      sessionStorage.setItem(cachKey, JSON.stringify(tmp))
     })
   }
 
   let getHomeMatches = () => {
     api.getHomeGamesByTeamId(id).then(res => {
-      let tmp = {...state}
+      let tmp = { ...state }
       tmp.homeMatches = res.data
       tmp.show = "home"
       setState(tmp)
+      sessionStorage.setItem(cachKey, JSON.stringify(tmp))
     })
   }
 
@@ -79,7 +77,7 @@ function Team({ match: { params: { id } } }) {
     if (!state.homeMatches) {
       getHomeMatches()
     } else {
-      let tmp = {...state}
+      let tmp = { ...state }
       tmp.show = "home"
       setState(tmp)
     }
@@ -89,27 +87,27 @@ function Team({ match: { params: { id } } }) {
     if (!state.awayMatches) {
       getAwayMatches()
     } else {
-      let tmp = {...state}
+      let tmp = { ...state }
       tmp.show = "away"
       setState(tmp)
     }
   }
 
   let allMatches = () => {
-    let tmp = {...state}
+    let tmp = { ...state }
     tmp.show = "all"
     setState(tmp)
   }
 
   if (state.show == "all") {
     MatchGrid = state.matches.map((match, i) => (
-        <CardObj key={i} d={match} getMatchTwo={(x, y) => { return null }} />
-      ))
-  } else if (state.show == "home"){
+      <CardObj key={i} d={match} getMatchTwo={(x, y) => { return null }} />
+    ))
+  } else if (state.show == "home") {
     MatchGrid = state.homeMatches.map((match, i) => (
       <CardObj key={i} d={match} getMatchTwo={(x, y) => { return null }} />
     ))
-  } else if (state.show == "away"){
+  } else if (state.show == "away") {
     MatchGrid = state.awayMatches.map((match, i) => (
       <CardObj key={i} d={match} getMatchTwo={(x, y) => { return null }} />
     ))
@@ -121,7 +119,7 @@ function Team({ match: { params: { id } } }) {
       <div>
         {console.log(state.teamName)}
         <h1>{state.teamName}</h1>
-        <StandingTable items={state.standing} />
+        <LoadTable seasonId={state.seasonId} />
         <Button variant="contained" onClick={allMatches}>
           All Matches
         </Button>
@@ -131,8 +129,8 @@ function Team({ match: { params: { id } } }) {
         <Button variant="contained" onClick={awayMatches}>
           Away Matches
         </Button>
-        <Grid container spacing={2}>
-        {MatchGrid}
+        <Grid item container xs={12}>
+          {MatchGrid}
         </Grid>
 
       </div>
