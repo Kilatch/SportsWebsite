6e5f8fc8ee1585e4fba22d1d4d33e1b3b47ce53d
James Morgan <james.d.morgan9830@gmail.com>
2020-10-28 14:48:48 +0100

    league shows season

--- src/Components/League.js
+++ src/Components/League.js
@@ -1,8 +1,47 @@
-import React from 'react'
-import SeasonsForLeague from './Sport/SeasonsForLeague'
+import React, { useState, useEffect } from 'react'
+import api from './api'
+import { Link } from 'react-router-dom'
+import './styles/SportLeagueList.css'
 
-const League = ({ match: { params } }) => {
-  return <SeasonsForLeague leagueId={params.id} />
+export default function League({ match: { params: { id } } }) {
+
+  const [state, setState] = useState({
+    error: null,
+    isLoaded: false,
+    leagueId: id,
+    leagueName: '',
+    seasons: []
+  })
+
+  useEffect(() => {
+    api.getSeasonsByLeague(id).then(res => {
+      let tmp = {...state}
+      tmp.isLoaded = true
+      if (res.data.length > 0) {
+        tmp.leagueName = res.data[0].league.name
+      } else {
+        tmp.leagueName = "Error loading league name"
+      }
+      tmp.seasons = res.data
+      setState(tmp)
+    })
+  }, [])
+
+  if (state.error) {
+    return <h1>error.message</h1>
+  } else if (!state.isLoaded) {
+    return <h1>Loading...</h1>
+  } else {
+    return (
+      <>
+        <h1 className="league-list">{state.leagueName}</h1>
+        <h2 className="season-year">Seasons:</h2>
+        {state.seasons.map((season,i)=>(
+          <Link key={i} to={"/season/"+season.id} >
+            <h2 className="season-year">{""+season.startYear +" - " + season.endYear}</h2>
+          </Link>
+        ))}
+      </>
+    )
+  }
 }
-
-export default League
