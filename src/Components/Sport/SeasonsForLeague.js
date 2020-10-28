357515b03010b98f672e53c6179af3cd8f361720
James Morgan <james.d.morgan9830@gmail.com>
2020-10-28 15:37:12 +0100

    league link from sport

--- src/Components/Sport/SeasonsForLeague.js
+++ src/Components/Sport/SeasonsForLeague.js
@@ -16,8 +16,8 @@ export default class SeasonsForLeague extends Component {
       leagueId: props.leagueId,
       error: null,
       items: [],
-      seasonToShow:null,
-      index:0
+      seasonToShow: null,
+      index: 0
     }
     this.updateComponent = this.updateComponent.bind(this)
     this.sortData = this.sortData.bind(this)
@@ -31,16 +31,18 @@ export default class SeasonsForLeague extends Component {
   componentDidMount() {
     this.updateComponent()
   }
-  updateSeason(value){
-    let len=this.state.items.length
-    if((value==0)&&this.state.index<len){
-      this.setState({index:this.state.index+1,
-        seasonToShow:this.state.items[this.state.index+1]
+  updateSeason(value) {
+    let len = this.state.items.length
+    if ((value == 0) && this.state.index < len) {
+      this.setState({
+        index: this.state.index + 1,
+        seasonToShow: this.state.items[this.state.index + 1]
       })
     }
-    else  if((value==1)&&this.state.index>0){
-      this.setState({index:this.state.index-1,
-        seasonToShow:this.state.items[this.state.index-1]
+    else if ((value == 1) && this.state.index > 0) {
+      this.setState({
+        index: this.state.index - 1,
+        seasonToShow: this.state.items[this.state.index - 1]
       })
     }
     console.log(value)
@@ -72,7 +74,9 @@ export default class SeasonsForLeague extends Component {
       let season = this.state.items[0]
       return (
         <div>
-          <h3 className="league-list"> {season.league.name} </h3>
+          <Link to={'/league/' + season.league.id}>
+            <h3 className="league-list"> {season.league.name} </h3>
+          </Link>
           <Link to={'/season/' + season.id}>
             <h4 className="season-year"> {season.startYear}</h4>
           </Link>
