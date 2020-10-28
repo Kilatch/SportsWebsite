64e2b6d4a11086ac6c4788df60dd0baf51fd826f
Ali Hussien <ali-18121@hotmail.com>
2020-10-28 13:33:21 +0100

    Updates

---
+++ precache-manifest.431a5e9e207156c64deeb2e03663a6f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3307b9039bf20ac3dd0c1d564a96414",
+    "url": "/SportsWebsite/index.html"
+  },
+  {
+    "revision": "483c1bb82c9f44d186be",
+    "url": "/SportsWebsite/static/css/main.a9e21873.chunk.css"
+  },
+  {
+    "revision": "1c148342f0dc356e6395",
+    "url": "/SportsWebsite/static/js/2.2fec3690.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/SportsWebsite/static/js/2.2fec3690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483c1bb82c9f44d186be",
+    "url": "/SportsWebsite/static/js/main.f310802c.chunk.js"
+  },
+  {
+    "revision": "d9edb7256004af8935dd",
+    "url": "/SportsWebsite/static/js/runtime-main.a2276727.js"
+  },
+  {
+    "revision": "752f43e3e046fa7b2d010f2a2e8935d6",
+    "url": "/SportsWebsite/static/media/bg.752f43e3.jpg"
+  },
+  {
+    "revision": "81ee673f90c372986464330d12d29af3",
+    "url": "/SportsWebsite/static/media/s.81ee673f.jpg"
+  }
+]);
