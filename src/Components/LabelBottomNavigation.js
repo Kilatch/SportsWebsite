55f3754273690be0d0f5843b2bd20de7fbc571ad
Ali Hussien <ali-18121@hotmail.com>
2020-10-29 11:18:09 +0100

    fixed z index in sports page and also added tooltip to arrows

--- src/Components/LabelBottomNavigation.js
+++ src/Components/LabelBottomNavigation.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import RestoreIcon from '@material-ui/icons/Restore'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
+import Tooltip from '@material-ui/core/Tooltip'
 
 const useStyles = makeStyles({
   root: {
@@ -16,25 +16,38 @@ const useStyles = makeStyles({
     color: 'white',
     height: 48,
     padding: '0 30px',
-
   },
-});
+})
 
-export default function LabelBottomNavigation({updateSeason}) {
-  const classes = useStyles();
-  const [value, setValue] = React.useState('');
+export default function LabelBottomNavigation({ updateSeason }) {
+  const classes = useStyles()
+  const [value, setValue] = React.useState('')
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    setValue(newValue)
     updateSeason(newValue)
-
-  };
+  }
 
   return (
-    <BottomNavigation value={value} onChange={(event,newValue)=>handleChange(event,newValue)} className={classes.root}>
-      <BottomNavigationAction  label="backward" value={0} icon={<ArrowBackIosIcon/>} />
-      <BottomNavigationAction  label="forward" value={1} icon={<ArrowForwardIosIcon/>} />
-     
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => handleChange(event, newValue)}
+      className={classes.root}
+    >
+      <Tooltip title="Previous Season">
+        <BottomNavigationAction
+          label="backward"
+          value={0}
+          icon={<ArrowBackIosIcon />}
+        />
+      </Tooltip>
+      <Tooltip title="Next Season">
+        <BottomNavigationAction
+          label="forward"
+          value={1}
+          icon={<ArrowForwardIosIcon />}
+        />
+      </Tooltip>
     </BottomNavigation>
-  );
+  )
 }
