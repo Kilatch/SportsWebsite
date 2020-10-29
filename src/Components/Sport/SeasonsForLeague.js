6505d7f90691afd771c5bf6ed03d2e759365601f
huseinmb91 <huseinmb4@gmail.com>
2020-10-29 10:36:36 +0100

    last

--- src/Components/Sport/SeasonsForLeague.js
+++ src/Components/Sport/SeasonsForLeague.js
@@ -6,6 +6,8 @@ import api from '../api'
 import '../styles/SportLeagueList.css'
 import LoadTable from '../LoadTable'
 import { Link } from 'react-router-dom'
+import { Button } from '@material-ui/core'
+import LoadBottomNav from '../LabelBottomNavigation'
 
 export default class SeasonsForLeague extends Component {
   constructor(props) {
@@ -14,8 +16,8 @@ export default class SeasonsForLeague extends Component {
       leagueId: props.leagueId,
       error: null,
       items: [],
-      seasonToShow: null,
-      index: 0,
+      seasonToShow:null,
+      index:0
     }
     this.updateComponent = this.updateComponent.bind(this)
     this.sortData = this.sortData.bind(this)
@@ -29,19 +31,19 @@ export default class SeasonsForLeague extends Component {
   componentDidMount() {
     this.updateComponent()
   }
-  updateSeason(value) {
-    let len = this.state.items.length
-    if (value === 0 && this.state.index < len) {
-      this.setState({
-        index: this.state.index + 1,
-        seasonToShow: this.state.items[this.state.index + 1],
-      })
-    } else if (value === 1 && this.state.index > 0) {
-      this.setState({
-        index: this.state.index - 1,
-        seasonToShow: this.state.items[this.state.index - 1],
+  updateSeason(value){
+    let len=this.state.items.length
+    if((value==0)&&this.state.index<len){
+      this.setState({index:this.state.index+1,
+        seasonToShow:this.state.items[this.state.index+1]
       })
     }
+    else  if((value==1)&&this.state.index>0){
+      this.setState({index:this.state.index-1,
+        seasonToShow:this.state.items[this.state.index-1]
+      })
+    }
+    console.log(value)
   }
 
   updateComponent() {
@@ -51,6 +53,7 @@ export default class SeasonsForLeague extends Component {
           items: res.data,
           leagueId: this.props.leagueId,
         })
+        console.log(this.state.items)
       },
       (error) => {
         this.setState({
@@ -61,23 +64,32 @@ export default class SeasonsForLeague extends Component {
   }
 
   render() {
-    if (this.props.leagueId !== this.state.leagueId) {
+    if (this.props.leagueId != this.state.leagueId) {
       this.updateComponent()
     }
     if (this.state.items.length > 0) {
       this.sortData()
-      let season = this.state.items[0]
-      return (
-        <div>
-          <Link to={'/league/' + season.league.id}>
+
+      let season ;
+      if(this.state.seasonToShow==null)season = this.state.items[0];
+      else{season=this.state.seasonToShow}
+
+      
+        return (
+         
+          <div>
+           
             <h3 className="league-list"> {season.league.name} </h3>
-          </Link>
-          <Link to={'/season/' + season.id}>
-            <h4 className="season-year"> {season.startYear}</h4>
-          </Link>
-          <LoadTable seasonId={season.id} />
-        </div>
-      )
+            <Link to={'/season/' + season.id}>
+              <h4 className="season-year"> {season.startYear}</h4>
+            </Link>
+           
+            <LoadTable seasonId={season.id} />
+            <LoadBottomNav updateSeason={this.updateSeason} />
+          </div>
+        )
+      
+
     } else return null
   }
 }
